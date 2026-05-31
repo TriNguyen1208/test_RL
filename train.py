6a2dba3aebffa673e631293c971d8ad608d56513
TriNguyen1208 <ductri0981@gmail.com>
2026-05-31 16:31:26 +0700

    Nothing

--- train.py
+++ train.py
@@ -240,4 +240,4 @@ if __name__ == "__main__":
                 my_trainer.save_model("model.pth")
             pbar.update(1)
             # pbar.set_postfix(step=f"{step:.2f}", episode_reward=f"{episode_reward:.3f}", win=f"{"YES" if is_win else "NO"}", memory=f"{len(my_trainer.memory)}/50000", epsilon=f"{my_trainer.epsilon:.3f}")
-            pbar.set_postfix(step=f"{step:.2f}", episode_reward=f"{episode_reward:.3f}", win=f"{"YES" if is_win else "NO"}")
+            pbar.set_postfix(step=f"{step:.2f}", reward=f"{episode_reward:.3f}", win=f"{"Y" if is_win else "N"}")
